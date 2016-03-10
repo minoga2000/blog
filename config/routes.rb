ea11549d3bc68c655c3a2a79242f21eb3369ab01
minoga2000 <minoga2000@gmail.com>
2016-03-10 14:17:12 +0300

    Add terms and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
  Rails.application.routes.draw do
   get 'home/index'
   get '/' => 'home#index'
-
+  
+  get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
   #get 'contacts' => 'contacts#new'   как вариант
   #resource :contacts, only: [:create]
 
