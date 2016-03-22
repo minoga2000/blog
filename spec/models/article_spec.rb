f73a1645fe4d7d82584eb2c4c8a4640d61ce16e8
minoga2000 <minoga2000@gmail.com>
2016-03-22 15:30:08 +0300

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,5 +9,15 @@ describe Article do
   describe "associations" do
   it { should have_many (:comments) }
   end
+
+  describe "#subject" do
+    it "returns the article title" do
+      # создаем объект article хитрым способом
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert, проверка
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
   
 end
