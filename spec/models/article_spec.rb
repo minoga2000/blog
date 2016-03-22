d396c91de1f432874c003ec6615682bfc7e23860
minoga2000 <minoga2000@gmail.com>
2016-03-22 12:16:44 +0300

    Update article_spec to include validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,6 +1,13 @@
 require "spec_helper"
 
 describe Article do 
+  describe "validations" do
+    it { should validate_presence_of (:title) }
+    it { should validate_presence_of (:text) }
+  end
+
+  describe "associations" do
   it { should have_many (:comments) }
-  #it { should validate_presence_of (:message) }
+  end
+  
 end
